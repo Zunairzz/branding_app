625b45941c3fb9ab3f6c79527dd40217fa4b5db1
Zunair Sarwar <92993165+Zunairzz@users.noreply.github.com>
2023-03-13 22:57:16 +0500

    Initial Commit
    Branding App

---
+++ src/main/java/com/example/branding_app/BrandingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.branding_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrandingAppApplication.class, args);
+    }
+
+}
